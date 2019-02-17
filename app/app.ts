0af9ad8b79caf06bf0a2473d6cc0faf426613e91
Chong Feng <chong.feng@koresoftware.com>
2019-02-17 23:17:04 +1100

    Add function

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,33 @@
 function startGame() {
-    var msgEl = document.getElementById('messages');
-    msgEl.innerHTML = 'welcome to the math game frome Chong';
+
+    let playerName: string | undefined = getInputValue('playername');
+
+    logPlayer(playerName);
+
+    let msgEl: HTMLElement | null = document.getElementById('messages');
+    msgEl!.innerHTML = 'welcome to the math game frome Chong Feng';
     console.log("test math game");
+
+    postScore(80);
 }
 
-document.getElementById('startGame').addEventListener('click', startGame);
+document.getElementById('startGame')!.addEventListener('click', startGame);
 
+function logPlayer(name: string = 'MultiMath Player'): void {
+    console.log(`new game starting for player: ${name}`);
+} 
+
+function getInputValue(elementId: string): string | undefined {
+    let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementId);
+
+    if(inputElement.value === '') {
+        return undefined;
+    }else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName: string = 'multimath player' ): void {
+    let scoreElement: HTMLElement | null = document.getElementById('postedScores');
+    scoreElement!.innerHTML = `${score} - ${playerName}`;
+}
--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 function startGame() {
+    var playerName = getInputValue('playername');
+    logPlayer(playerName);
     var msgEl = document.getElementById('messages');
-    msgEl.innerHTML = 'welcome to the math game frome Chong';
+    msgEl.innerHTML = 'welcome to the math game frome Chong Feng';
     console.log("test math game");
+    postScore(80);
 }
 document.getElementById('startGame').addEventListener('click', startGame);
+function logPlayer(name) {
+    if (name === void 0) { name = 'MultiMath Player'; }
+    console.log("new game starting for player: " + name);
+}
+function getInputValue(elementId) {
+    var inputElement = document.getElementById(elementId);
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+function postScore(score, playerName) {
+    if (playerName === void 0) { playerName = 'multimath player'; }
+    var scoreElement = document.getElementById('postedScores');
+    scoreElement.innerHTML = score + " - " + playerName;
+}
 //# sourceMappingURL=app.js.map
